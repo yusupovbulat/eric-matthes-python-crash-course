801ee0f6f2ae6a9e8d515451e91d4552d85037e8
Bulat Yusupov <yusupovbulat@gmail.com>
2018-07-14 17:02:22 +0500

    Exersice 13 from chapter 4

---
+++ chapter4/4-13.py
@@ -0,0 +1,8 @@
+print('Buffet menu:')
+dishes = ('pizza', 'falafel', 'carrot cake', 'cannoli', 'ice cream')
+for dish in dishes:
+    print(dish.title())
+print('\nNew buffet menu:')
+dishes = ('pizza', 'burger', 'cheesecake', 'cannoli', 'ice cream')
+for dish in dishes:
+    print(dish.title())
