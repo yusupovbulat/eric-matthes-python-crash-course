f930ca33285b6dcbe4d51807db0506d7dd6864b7
Bulat Yusupov <yusupovbulat@gmail.com>
2018-07-15 21:55:19 +0500

    Modified example from ch.1 pt.6

--- chapter_1_basics/part6/favorite_languages.py
+++ chapter_1_basics/part6/favorite_languages.py
@@ -17,3 +17,23 @@ for name in favorite_laguages.keys():
     if name in frinds:
         print(" Hi " + name.title() + ", I see you favorite language is " +
               favorite_laguages[name].title() + "!")
+print()
+favorite_laguages = {
+        'jen': 'python',
+        'sarah': 'c',
+        'edward': 'ruby',
+        'phil': 'python',
+        }
+for name in sorted(favorite_laguages.keys()):
+    print(name.title() + ", thank you for talking the poll.")
+
+print()
+favorite_laguages = {
+        'jen': 'python',
+        'sarah': 'c',
+        'edward': 'ruby',
+        'phil': 'python',
+        }
+print('The following languages have been mentioned:')
+for language in favorite_laguages.values():
+    print(language.title())
