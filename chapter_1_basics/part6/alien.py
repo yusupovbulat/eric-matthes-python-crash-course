1a181e592e42132c23a9a2b14b5cef79da60daae
Bulat Yusupov <yusupovbulat@gmail.com>
2018-07-16 20:53:25 +0500

    Modify example

--- chapter_1_basics/part6/alien.py
+++ chapter_1_basics/part6/alien.py
@@ -1,25 +1,7 @@
 alien_0 = {'color': 'green', 'points': 5}
-print(alien_0['color'])
-print(alien_0['points'])
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
 
-alien_0 = {}
-alien_0['color'] = 'green'
-alien_0['points'] = 5
-print(alien_0)
-
-alien_0 = {'color': 'green'}
-print('The alien is ' + alien_0['color'] + '.')
-
-alien_0['color'] = 'yellow'
-print('The alien is ' + alien_0['color'] + '.')
-alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
-print('Original x-position: ' + str(alien_0['x_position']))
-
-if alien_0['speed'] == 'slow':
-    x_increment = 1
-elif alien_0['speed'] == 'medium':
-    x_increment = 2
-else:
-    x_increment = 3
-alien_0['x_position'] = alien_0['x_position'] + x_increment
-print('New x-position: ' + str(alien_0['x_position']))
+for alien in aliens:
+    print(alien)
