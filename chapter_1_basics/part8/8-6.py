3bb5e18f3fbcbb5ad0629906f90f22c31562f2cf
Bulat Yusupov <yusupovbulat@gmail.com>
2018-07-19 08:47:04 +0500

    Solved exercise 6 from ch.1 pt.8

---
+++ chapter_1_basics/part8/8-6.py
@@ -0,0 +1,6 @@
+def print_city_country(city, country):
+    print(city.title() + ", " + country.title())
+
+print_city_country('belgorod', 'russia')
+print_city_country('santiago', 'chilie')
+print_city_country('reykjavik', 'iceland')
