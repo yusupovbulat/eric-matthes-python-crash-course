194927813a9e2a21ab8a82326556831f9d5514b7
Bulat Yusupov <yusupovbulat@gmail.com>
2018-07-21 08:48:43 +0500

    Example without methods

---
+++ chapter_1_basics/part8/printing_models.py
@@ -0,0 +1,16 @@
+# Список моделей, которые необходимо напечатать
+unprinting_designs = ['iphone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+# Цикл последовательно печатает каждую модель до конца списка.
+# После печати каждая модель перемещается в список completed_models.
+while unprinting_designs:
+    current_design = unprinting_designs.pop()
+    # Печать модели на 3D-принтере
+    print('Printing models: ' + current_design)
+    completed_models.append(current_design)
+
+# Вывод всех готовых моделей.
+print('\nThe following models have been printed:')
+for completed_model in completed_models:
+    print(completed_model)
