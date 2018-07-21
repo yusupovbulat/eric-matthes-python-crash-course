9898b7da971e5ca45c5b699b42195327c50cb915
Bulat Yusupov <yusupovbulat@gmail.com>
2018-07-21 21:09:15 +0500

    Refactoring examples. Using 'import'

--- chapter_1_basics/part8/pizza.py
+++ chapter_1_basics/part8/pizza.py
@@ -4,7 +4,3 @@ def make_pizza(size, *toppings):
           '-inch pizza with the follwing toppings:')
     for topping in toppings:
         print('- ' + topping)
-
-
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
