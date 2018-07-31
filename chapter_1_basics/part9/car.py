0c9cff50e7958fdf774b56200ca5a795b9647821
Bulat Yusupov <yusupovbulat@gmail.com>
2018-07-31 23:58:25 +0500

    Add to car.py classes Battery and ElectricCar

--- chapter_1_basics/part9/car.py
+++ chapter_1_basics/part9/car.py
@@ -29,3 +29,33 @@ class Car():
     def increment_odometer(self, miles):
         """Увеличение показания одометра с заданным приращением"""
         self.odometer_reading += miles
+
+class Battery():
+    """Simple model electric car battery"""
+    def __init__(self, battery_size=60):
+        """Initialization battery atributes"""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Show information about battery power."""
+        print('This car has a ' + str(self.battery_size) + '-kWh battery.')
+
+    def get_range(self):
+        """Displays approximate run reserve for the battery"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = 'This car can go approximately ' + str(range)
+        message += ' miles on a full charge.'
+        print(message)
+
+class ElectricCar(Car):
+    """Specific aspects for electric cars"""
+    def __init__(self, make, model, year):
+        """Initialization atribute from superclass.
+        Then initializate specific atribute for electic cars"""
+        super().__init__(make, model, year)
+        self.battery_size = Battery()
+
